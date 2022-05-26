5a23e30f064251ab794eb8c414320bdfd4e574b1
Jaime Aranaz <jaime.aranaz@gmail.com>
2022-05-26 17:03:51 +0200

    Filter parsed data

--- AxaTest/AxaTest/views/ViewController.swift
+++ AxaTest/AxaTest/views/ViewController.swift
@@ -35,5 +35,57 @@ class ViewController: UIViewController {
             }
             .store(in: &subscribers)
     }
+
+    private func getFilterValues(fromCharacters characters: [Character]) {
+        
+        var professions = Set<String>()
+        var hairColors = Set<String>()
+        
+        characters.forEach ({
+            $0.professions.forEach { professions.insert($0) }
+            hairColors.insert($0.hairColor)
+        })
+        
+        let minAge = characters.min { $0.age < $1.age }?.age
+        let maxAge = characters.max { $0.age < $1.age }?.age
+        
+        let minWeight = characters.min { $0.weight < $1.weight }?.weight
+        let maxWeight = characters.max { $0.weight < $1.weight }?.weight
+        
+        let minHeight = characters.min { $0.height < $1.height }?.height
+        let maxHeight = characters.max { $0.height < $1.height }?.height
+    }
+
+    private func filter(characters: [Character]) -> [Character] {
+        
+        let hairColors = ["Black", "White", "Red", "Gray", "Red", "Pink" ]
+        let professions = ["Stonecarver", "Tinker", "Carpenter", "Brewer", "Farmer"]
+        let selectedHairColors: [String] = [String]()
+        let selectedProfessions = ["Tinker", "Carpenter", "Brewer"]
+
+        let filtered = characters
+            .filter { (1...50).contains($0.age) }
+            .filter { (1...500).contains($0.weight) }
+            .filter { (1...500).contains($0.height) }
+            .filter ({
+                if selectedHairColors.isEmpty {
+                    return true
+                } else {
+                    return selectedHairColors.contains($0.hairColor)
+                }
+            })
+            .filter ({
+                if selectedProfessions.isEmpty {
+                    return true
+                } else {
+                    let professionsSet = Set($0.professions)
+                    let selectedProfessionsSet = Set(selectedProfessions)
+                    let inBoth = professionsSet.intersection(selectedProfessionsSet)
+                    return inBoth.isNotEmpty
+                }
+            })
+        
+        return filtered
+    }
 }
 
